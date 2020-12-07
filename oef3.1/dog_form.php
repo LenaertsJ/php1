76d20969e32736d8f4b1ac58e9c0a633680ec1a7
Julie Lenaerts <julielenaerts@gmail.com>
2020-12-07 19:46:29 +0100

    save.php

--- oef3.1/dog_form.php
+++ oef3.1/dog_form.php
@@ -28,50 +28,23 @@ printJumbo("Zumi", "Edit photo");
             print '<br>';
             print '<br>';
         }
-        ?>
 
+        ?>
     </div>
 </div>
 
-<form style="width: 75%; padding-left: 100px;" id="edit_form" name="edit_form" method="POST" action="save.php">
-    <div class="form-group row">
-        <label for="staticEmail" class="col-sm-2 col-form-label">ID</label>
-        <div class="col-sm-10">
-            <input type="text" readonly class="form-control-plaintext" id="img_id" name="img_id" value= "1">
-        </div>
-    </div>
-    <div class="form-group row">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Titel</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="img_title" name="img_title" placeholder="Give new title..">
-        </div>
-    </div>
-    <div class="form-group row">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Filename</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="img_filename" name="img_filename" placeholder="Adjust filename...">
-        </div>
-    </div>
-    <div class="form-group row">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Width</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="img_width" name="img_width" placeholder="Adjust width...">
-        </div>
-    </div>
-    <div class="form-group row">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Height</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="img_height" name="img_height" placeholder="Adjust height...">
-        </div>
-    </div>
-    <button type="submit" class="btn btn-primary mb-2">Save</button>
-</form>
+    <?php
+    // GET FORM TEMPLATE
+    $html = file_get_contents("templates/form.html");
+    // GET DATA
+    $rows = getData($sql);
+    // MERGE DATA WITH TEMPLATE
+    buildHTML($html, $rows);
+    ?>
+
 <br>
 <br>
 <p style="text-align: center; margin-bottom: 50px;"><a href=dog2.php>Back to portfolio</a></p>
-
-
-
 
 </body>
 </html>
--- oef3.1/save.php
+++ oef3.1/save.php
@@ -1,6 +1,16 @@
 <?php
 
-print json_encode($_POST);
+require_once "lib/connection.php";
+global $conn;
 
+$sql = "UPDATE images SET ";
+$sql .= "img_title = '" . $_POST['img_title'] . "', ";
+$sql .= "img_filename = '" . $_POST['img_filename'] . "', ";
+$sql .= "img_width = '" . $_POST['img_width'] . "', ";
+$sql .= "img_height = '" . $_POST['img_height'] . "' ";
+$sql .= "WHERE img_id = '" . $_POST['img_id'] . "'; ";
 
-?>
+$result = $conn->query($sql);
+
+header("Location: dog2.php");
+
